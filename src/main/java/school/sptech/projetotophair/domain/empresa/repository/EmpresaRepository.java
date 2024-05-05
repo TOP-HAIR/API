19812748e2ab2c37ff5b09f079959f42e3530894
Caio Ruiz de Souza <110926353+CaioRuizdeSouza@users.noreply.github.com>
2024-05-05 17:40:17 -0300

    Alteracao back17

--- src/main/java/school/sptech/projetotophair/domain/empresa/repository/EmpresaRepository.java
+++ src/main/java/school/sptech/projetotophair/domain/empresa/repository/EmpresaRepository.java
@@ -25,4 +25,24 @@ public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
             "ORDER BY AVG(a.nivel) DESC")
     List<Empresa> findTopEmpresasMelhorAvaliadasPorEstado(@Param("estado") String estado);
 
+
+    @Query("SELECT DISTINCT s.empresa, AVG(a.nivel) as avgNivel FROM Servico s " +
+            "LEFT JOIN s.empresa e " +
+            "LEFT JOIN e.avaliacoes a " +
+            "LEFT JOIN e.endereco endereco " +
+            "WHERE (:estado IS NULL OR endereco.estado LIKE %:estado%) " +
+            "AND (:nomeServico IS NULL OR s.nomeServico LIKE %:nomeServico%) " +
+            "AND (:nomeEmpresa IS NULL OR e.razaoSocial LIKE %:nomeEmpresa%) " +
+            "AND (:usuarioId IS NULL OR EXISTS (SELECT u FROM e.usuarios u WHERE u.id = :usuarioId)) " +
+            "GROUP BY e " +
+            "ORDER BY avgNivel DESC")
+    List<Object[]> findEmpresasByFiltros(
+            @Param("estado") String estado,
+            @Param("nomeServico") String nomeServico,
+            @Param("nomeEmpresa") String nomeEmpresa,
+            @Param("usuarioId") Long usuarioId);
+
+
+
+
 }
